e226cc07fd89d7e0ae579cb30031016676cc04ba
asmae <asmaeaouassar333@example.com>
2025-07-06 13:21:39 +0100

    add a main code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 
 import { Component } from '@angular/core';
+import { MainpageComponent } from './main/mainpage/mainpage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MainpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
